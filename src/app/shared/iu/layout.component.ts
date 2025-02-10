cb42c5f88c98eed0f0da0eaeea6c0b454896b591
albertomanuel.cahuiche <albertomanuel.cahuiche@gmail.com>
2025-02-10 22:57:20 +0000

    Se agregan iconos a los botones

--- src/app/shared/iu/layout.component.ts
+++ src/app/shared/iu/layout.component.ts
@@ -10,7 +10,16 @@ import { AuthStateService } from "../data-access/auth-state.service";
     <header class="h-[80px] mb-8 w-full max-w-screen-lg mx-auto px-4">
       <nav class="flex items-center justify-between h-full">
       <a class="text-2xl font-bold" routerLink="/tasks"> Registro de sitios importantes</a>
-      <button (click)="logOut()" type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Cerrar Sesion</button>
+      <button (click)="logOut()" type="button"
+    class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 inline-flex items-center">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="me-2 icon icon-tabler icons-tabler-outline icon-tabler-logout-2">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M10 8v-2a2 2 0 0 1 2 -2h7a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-2" />
+        <path d="M15 12h-12l3 -3" />
+        <path d="M6 15l-3 -3" />
+    </svg>
+    Cerrar Sesión
+</button>
       </nav>
     </header>
     <router-outlet />

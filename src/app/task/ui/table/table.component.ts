fea86e7b5323b762aae384b5fea325b4ad53f159
albertomanuel.cahuiche <albertomanuel.cahuiche@gmail.com>
2025-02-13 18:26:39 +0000

    Se agrega campo de busqueda

--- src/app/task/ui/table/table.component.ts
+++ src/app/task/ui/table/table.component.ts
@@ -1,19 +1,30 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, computed, inject, input, signal } from '@angular/core';
 import { Task, TaskService } from '../../data-access/task.service';
 import { RouterLink } from '@angular/router';
 import { toast } from 'ngx-sonner';
 import { TruncatePipe } from '../../../core/truncate.pipe';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [RouterLink, TruncatePipe],
+  imports: [RouterLink, TruncatePipe, FormsModule],
   templateUrl: './table.component.html',
   styles: ``
 })
 export class TableComponent {
   tasks = input.required<Task[]>();
   private _taskService = inject(TaskService);
+
+  searchTerm = signal<string>('');
+
+  filteredTasks = computed(() => {
+    const term = this.searchTerm().toLowerCase().trim();
+    return this.tasks().filter(task => 
+      task.title.toLowerCase().includes(term) || 
+      task.description.toLowerCase().includes(term)
+    );
+  });
 
   deleteSite(id:string) {
     this._taskService.delete(id);
